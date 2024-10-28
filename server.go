b8e759efdc9aaea88b34fb10a267104d851f8e78
Erfei Yu <105307186+454270186@users.noreply.github.com>
2024-10-29 00:32:38 +0800

    multi functions:

--- server.go
+++ server.go
@@ -19,7 +19,12 @@ func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
+func greet2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World22222")
+}
+
 func main() {
-	http.HandleFunc("/", greet)
+	http.HandleFunc("/api/hello", greet)
+	http.HandleFunc("/api/hello2", greet2)
 	http.ListenAndServe(get_port(), nil)
 }
